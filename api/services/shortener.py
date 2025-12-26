e01b005a3f5319c83e24e0a2db8b332d3620eeea
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-12-26 09:57:52 +0300

    Fix async

--- api/services/shortener.py
+++ api/services/shortener.py
@@ -4,14 +4,29 @@ from utils import generate_short_code
 from cache import url_cache, UrlCacheModel
 
 
-async def create_short(db, long_url, custom_code=None, preview_type=None) -> ShortURL:
-    short = ShortURL(
-        long_url=long_url,
-        custom=bool(custom_code),
-        preview_type=preview_type,
-        short_code=custom_code or generate_short_code(),
+from sqlalchemy.ext.asyncio import AsyncSession
+
+async def create_short(
+    db: AsyncSession,
+    long_url: str,
+    custom_code: str | None = None,
+    preview_type: str | None = None,
+) -> ShortURL:
+    async with db.no_autoflush:
+        short = ShortURL(
+            long_url=long_url,
+            custom=bool(custom_code),
+            preview_type=preview_type,
+            short_code=custom_code or generate_short_code(),
+        )
+        db.add(short)
+
+    await db.commit()
+    await db.refresh(short)
+
+    url_cache[short.short_code] = UrlCacheModel(
+        id=short.id,
+        url=long_url,
     )
-    db.add(short)
-    await db.flush()
-    url_cache[short.short_code] = UrlCacheModel(id=short.id, url=long_url)
+
     return short
