a96cf614d6dd1aab8ca0af85f2b349def075ac60
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-12-26 07:59:49 +0300

    Change

--- api/db.py
+++ api/db.py
@@ -1,10 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
-# from config import config
-#
-# DATABASE_URL = config.get("DATABASE_URL")
+from config import DATABASE_URL
 
-DATABASE_URL = 'postgresql+asyncpg://postgres:postgres@localhost:1000/urlshort'
+
+# DATABASE_URL = 'postgresql+asyncpg://postgres:postgres@localhost:1000/urlshort'
 
 engine = create_async_engine(
     DATABASE_URL,
